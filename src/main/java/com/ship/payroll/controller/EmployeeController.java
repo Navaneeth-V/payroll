e539a27fdb4d8e6e533e5b21d94711a863c6e978
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-08-11 20:55:15 +0530

    rest controller

--- src/main/java/com/ship/payroll/controller/EmployeeController.java
+++ src/main/java/com/ship/payroll/controller/EmployeeController.java
@@ -3,9 +3,9 @@ package com.ship.payroll.controller;
 import com.ship.payroll.model.Employee;
 import com.ship.payroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
@@ -18,5 +18,27 @@ class EmployeeController {
     public Iterable<Employee> getEmployees() {
         // sample API
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployeeById(@PathVariable String id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable String id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<Object> deleteEmployeeById(@PathVariable String id) {
+        employeeService.deleteEmployeeById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
