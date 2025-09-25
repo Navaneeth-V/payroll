1b63639822c3dda5e4ace276d5ac14ce7a26bae2
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-09-25 23:33:19 +0530

    methodArgumentNotValidException

--- src/main/java/com/employee/payroll/exception/GlobalExceptionHandler.java
+++ src/main/java/com/employee/payroll/exception/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package com.employee.payroll.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +28,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<Object> handleSQLException(SQLException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            body.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
