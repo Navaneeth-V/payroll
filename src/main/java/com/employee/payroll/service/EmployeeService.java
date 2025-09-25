008f64e3de99390ed4f67f61a00d1791831fadd2
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-09-25 23:00:03 +0530

    logger

--- src/main/java/com/employee/payroll/service/EmployeeService.java
+++ src/main/java/com/employee/payroll/service/EmployeeService.java
@@ -3,6 +3,8 @@ package com.employee.payroll.service;
 import com.employee.payroll.model.Employee;
 import com.employee.payroll.repository.EmployeeRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,12 +13,14 @@ import java.util.Optional;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
 
     EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
     public Employee addEmployee(Employee employee) {
+        LOGGER.info("Adding employee {}", employee);
         return this.employeeRepository.save(employee);
     }
 
@@ -33,14 +37,17 @@ public class EmployeeService {
         Optional<Employee> optionalEmployee = employeeRepository.findById(String.valueOf(employee.getId()));
 
         if(optionalEmployee.isPresent()) {
+            LOGGER.info("Updating employee {}", employee);
             return employeeRepository.save(employee);
         } else {
+            LOGGER.info("Employee with id {} not found", employee.getId());
             return null;
         }
     }
 
     public void deleteEmployeeById(String id) {
         this.employeeRepository.deleteById(id);
+        LOGGER.info("Employee with id {} deleted", id);
     }
 
 }
